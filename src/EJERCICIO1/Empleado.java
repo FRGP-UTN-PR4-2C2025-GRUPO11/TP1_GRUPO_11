1f50f9cf220fc5c919fc0407796c8fb18ed79947
GabrielErizaga <gabriel.erizaga@alumnos.frgp.utn.edu.ar>
2025-08-16 15:21:35 -0300

    Variable contador

--- src/EJERCICIO1/Empleado.java
+++ src/EJERCICIO1/Empleado.java
@@ -1,30 +1,34 @@
 package EJERCICIO1;
 
 public class Empleado extends Persona {
-	private int legajo;
+	private final int legajo;
 	private String puesto;
+	static int contador=1000;
 
 	// Constructors:
 	public Empleado() {
 		super();
+		contador++;
+		this.legajo = contador;
 	}
 
 	public Empleado(String dni, String nombre, String apellido, java.time.LocalDate
 			fechaNacimiento, String genero, String direccion, String telefono, String email,
-			int legajo, String puesto) {
+			 String puesto) {
 		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
-		this.legajo = legajo;
+		contador++;
+		this.legajo = contador;
 		this.puesto = puesto;
 	}
-
+	
 	// Gettters and setters
 	public int getLegajo() {
 		return legajo;
 	}
 
-	public void setLegajo(int legajo) {
-		this.legajo = legajo;
-	}
+	///public void setLegajo(int legajo) {
+	///	this.legajo = legajo;
+	///}
 
 	public String getPuesto() {
 		return puesto;
